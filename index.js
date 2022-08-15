edf3ea80dc17f480cd5a5c3ac499a64f1c473616
polarity <Charles.ran9@gmail.com>
2022-08-15 16:36:17 -0400

    feat: environment initialization, parse problems from wiki, render to katex

---
+++ index.js
@@ -0,0 +1,57 @@
+import got from "got";
+import cheerio from "cheerio";
+import katex from "katex";
+/**
+interface Problem {
+    exam: string;
+    version: string;
+    author?: string;
+    difficulty?: number;
+    answer?: string;
+    link?: string;
+}
+*/
+const api = "https://artofproblemsolving.com/wiki/api.php?";
+const fetchWikiPage = async (page) => {
+    return JSON.parse((await got(api + "action=parse&format=json&page=" + page)).body).parse;
+};
+const isInt = (str) => {
+    return (!isNaN(str) &&
+        !isNaN(parseFloat(str)) &&
+        Number.isInteger(parseFloat(str)));
+};
+const parseWikiProblem = async (page) => {
+    const { text: { "*": wikiPage }, } = await fetchWikiPage(page);
+    let $ = cheerio.load(wikiPage);
+    let problemHTML = $('h2:has(span[id="Problem"])').nextUntil('p:has(a:contains("Solution")), h2');
+    let wikiProblem = Object.entries($(problemHTML))
+        .map((el) => {
+        if (!isInt(el[0])) {
+            return "";
+        }
+        let element = $(el[1]);
+        return `<${element["0"].name}>${element.html()}</${element["0"].name}>`;
+    })
+        .join("");
+    console.log(wikiProblem);
+    return wikiProblem;
+};
+const renderKatex = (htmlString) => {
+    let $ = cheerio.load(htmlString);
+    let latexImages = $('img.latexcenter,img.latex').replaceWith((index, el) => {
+        let latexSrc = $(el).attr('alt');
+        latexSrc = latexSrc.replaceAll('$', '').replaceAll('\\[', '').replaceAll('\\]', '');
+        console.log(latexSrc);
+        let newEl = katex.renderToString(latexSrc, {
+            throwOnError: false,
+            displayMode: $(el).attr('class') == 'latexcenter'
+        });
+        console.log(newEl);
+        return $(newEl);
+    });
+    return $.html();
+};
+(async () => {
+    const problem = await parseWikiProblem("2020_AMC_10A_Problems/Problem_8");
+    console.log(renderKatex(problem));
+})();
